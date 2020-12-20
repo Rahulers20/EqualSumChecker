528c6281065c9aff312421c1fc98d72e3ca13326
Rahul Ravishankar <rahulrshankar02@gmail.com>
2020-12-20 22:43:20 +0530

    Initial commit

---
+++ src/EqualSumChecker.java
@@ -0,0 +1,10 @@
+public class EqualSumChecker {
+
+    public static boolean hasEqualSum(int firstValue, int secondValue, int resultingValue) {
+        if(firstValue + secondValue == resultingValue) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
